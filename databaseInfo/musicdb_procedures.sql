f4c5bafe87aa9d379d4cb6d5cb62b34d9e3d9782
Nam Luu <namluu@Nams-MacBook-Pro.local>
2016-04-08 16:21:29 -0400

    fix stored procedures

--- databaseInfo/musicdb_procedures.sql
+++ databaseInfo/musicdb_procedures.sql
@@ -4,14 +4,21 @@ DELIMITER $$
 DROP PROCEDURE IF EXISTS find_user_by_username $$
 CREATE PROCEDURE find_user_by_username(IN uname VARCHAR(50))
 BEGIN
-	SELECT * FROM users
+	SELECT u.userid, u.firstname, u.lastname, u.password, u.email, u.roles, 
+		GROUP_CONCAT(l.songid SEPARATOR ', ') AS songs
+    FROM users u JOIN library l
+    ON u.userid = l.userid
     WHERE username = uname;
 END$$
 
 DROP PROCEDURE IF EXISTS find_all_user $$
 CREATE PROCEDURE find_all_user()
 BEGIN
-	SELECT * FROM users;
+	SELECT u.userid, u.firstname, u.lastname, u.password, u.email, u.roles, 
+		GROUP_CONCAT(l.songid SEPARATOR ', ') AS songs
+    FROM users u JOIN library l
+    ON u.userid = l.userid
+    GROUP BY u.userid;
 END$$
 
 DROP PROCEDURE IF EXISTS create_user $$
@@ -42,7 +49,11 @@ END$$
 DROP PROCEDURE IF EXISTS find_user_by_userid $$
 CREATE PROCEDURE find_user_by_userid(IN uid VARCHAR(50))
 BEGIN
-	SELECT * FROM users WHERE userid = uid;
+	SELECT u.userid, u.firstname, u.lastname, u.password, u.email, u.roles, 
+		GROUP_CONCAT(l.songid SEPARATOR ', ') AS songs
+    FROM users u JOIN library l
+    ON u.userid = l.userid
+    WHERE u.userid = uid;
 END$$
 
 DROP PROCEDURE IF EXISTS delete_user_song $$
